f7dce86b38c113f10068608d6fe723590b19bd34
F S <1209771961@qq.com>
2020-12-04 14:39:59 +0800

    qwer

--- baseProject/common/manager/SSfileManager.m
+++ baseProject/common/manager/SSfileManager.m
@@ -7,7 +7,6 @@
 //
 
 #import "SSfileManager.h"
-//#import "SSuserModel.h"
 
 @implementation SSfileManager
 
@@ -19,21 +18,72 @@ static SSfileManager* fileManager = nil;
     });
     return fileManager;
 }
+///沙盒中documents路径
+- (NSString*)SSdocumentsPath {
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+}
+
+///沙盒中library路径
+- (NSString*)SSlibraryPath {
+    return [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) lastObject];
+}
+
+///沙盒中caches路径
+- (NSString*)SScachesPath {
+    return [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject];
+}
+
+///沙盒中temp路径
+- (NSString*)SStempPath {
+    return NSTemporaryDirectory();
+}
 
 ///删除登录用户信息
-- (void)deleteUserInfo {
+- (void)SSclearNSuserDefault {
     [[NSUserDefaults standardUserDefaults] removeObjectForKey:token];
-//    [[SSuserModel shareModel] deleteInfo];
 }
 
 ///删除本地数据
-- (void)deleteInfo {
-//    [[NSUserDefaults standardUserDefaults] removeObjectForKey:""];
+- (void)SSclearDocuments {
+    NSArray* filePaths = [[NSFileManager defaultManager] subpathsAtPath:[self SSdocumentsPath]];
+    for (NSString* fileP in filePaths) {
+        NSError* error;
+        do {
+            [[NSFileManager defaultManager] removeItemAtPath:[[self SSdocumentsPath] stringByAppendingPathComponent:fileP] error:&error];
+        } while ([[NSFileManager defaultManager] fileExistsAtPath:[[self SSdocumentsPath] stringByAppendingPathComponent:fileP]]);
+    }
 }
 
 ///清空缓存
-- (void)deleteAllCache {
-    
+- (void)SSclearCache {
+    for (NSString* fileP in [[NSFileManager defaultManager] subpathsAtPath:[self SScachesPath]]) {
+        NSError* error;
+        do {
+            [[NSFileManager defaultManager] removeItemAtPath:[[self SScachesPath] stringByAppendingPathComponent:fileP] error:&error];
+        } while ([[NSFileManager defaultManager] fileExistsAtPath:[[self SScachesPath] stringByAppendingPathComponent:fileP]]);
+    }
+}
+
+- (unsigned long long)SSfileSizeForDocument {
+    unsigned long long size = 0;
+    if ([[[NSFileManager defaultManager] subpathsAtPath:[self SSdocumentsPath]] count] <= 0) {
+        return size;
+    }
+    for (NSString* fileP in [[NSFileManager defaultManager] subpathsAtPath:[self SSdocumentsPath]]) {
+        size += [[[NSFileManager defaultManager] attributesOfItemAtPath:[[self SSdocumentsPath] stringByAppendingPathComponent:fileP] error:nil] fileSize];
+    }
+    return size;
+}
+
+- (unsigned long long)SSfileSizeForCaches {
+    unsigned long long size = 0;
+    if ([[[NSFileManager defaultManager] subpathsAtPath:[self SScachesPath]] count] <= 0) {
+        return size;
+    }
+    for (NSString* fileP in [[NSFileManager defaultManager] subpathsAtPath:[self SScachesPath]]) {
+        size += [[[NSFileManager defaultManager] attributesOfItemAtPath:[[self SScachesPath] stringByAppendingPathComponent:fileP] error:nil] fileSize];
+    }
+    return size;
 }
 
 
