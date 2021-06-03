3a318a7677b9a840b7250f915f84836e45d54204
apple <>
2021-06-03 16:00:04 +0800

    qwer

---
+++ baseProject/common/view/SSCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  SSCollectionView.h
+//  baseProject
+//
+//  Created by apple on 2021/6/3.
+//  Copyright © 2021 FL S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SSCollectionView : UICollectionView
+
+@property(nonatomic,strong) NSMutableArray* ssDatas;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ baseProject/common/view/SSCollectionView.m
@@ -0,0 +1,50 @@
+//
+//  SSCollectionView.m
+//  baseProject
+//
+//  Created by apple on 2021/6/3.
+//  Copyright © 2021 FL S. All rights reserved.
+//
+
+#import "SSCollectionView.h"
+
+@interface SSCollectionView ()
+
+@end
+@implementation SSCollectionView
+
+- (instancetype)init {
+    if (self = [super init]) {
+        
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame collectionViewLayout:(UICollectionViewLayout *)layout {
+    if (self = [super initWithFrame:frame collectionViewLayout:layout]) {
+        
+    }
+    return self;
+}
+
+- (void)setSSCollectionView {
+    self.ssDatas = [NSMutableArray arrayWithCapacity:0];
+    
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
