7f9586c004c25dcba81e16fa60bab3abfed57b47
apple <>
2021-06-15 11:23:33 +0800

    qwer

--- baseProject/common/manager/SSfileManager.h
+++ baseProject/common/manager/SSfileManager.h
@@ -16,48 +16,56 @@ NS_ASSUME_NONNULL_BEGIN
 
 #pragma mark ------------- 路径 ---------------
 ///沙盒中documents路径
-- (NSString*)SSdocumentsPath;
++ (NSString*)SSdocumentsPath;
 
 ///沙盒中library路径
-- (NSString*)SSlibraryPath;
++ (NSString*)SSlibraryPath;
 
 ///沙盒中caches路径
-- (NSString*)SScachesPath;
++ (NSString*)SScachesPath;
 
 ///沙盒中temp路径
-- (NSString*)SStempPath;
++ (NSString*)SStempPath;
 
 #pragma mark ----------- 创建缓存文件夹/文件 -----------
 ///在caches路径下创建文件夹
-- (void)SScreateDirectoryWithCachesPath:(NSString*)directoryPath;
++ (void)SScreateDirectoryWithCachesPath:(NSString*)directoryPath;
 
 ///在caches路径下创建文件
-- (void)SScreateFileWithCachesPath:(NSString*)filePath;
++ (void)SScreateFileWithCachesPath:(NSString*)filePath;
 
 ///在document路径下创建文件夹
-- (void)SScreateDirectoryWithDocumentPath:(NSString*)directoryPath;
++ (void)SScreateDirectoryWithDocumentPath:(NSString*)directoryPath;
 
 ///在document路径下创建文件
-- (void)SScreateFileWithDocumentPath:(NSString*)filePath;
++ (void)SScreateFileWithDocumentPath:(NSString*)filePath;
+
+#pragma mark ----------- 写入数据到文件中 -------------
+///将文本数据写入文件中
++ (void)SSwriteStrToFile:(NSString*)textStr filePath:(NSString*)filePath;
+///将数组数据写入文件中
++ (void)SSwriteArrToFile:(NSArray*)arr filePath:(NSString*)filePath;
+///将字典数据写入文件中
++ (void)SSwirteDicToFile:(NSDictionary*)dic filePath:(NSString*)filePath;
 
 #pragma mark ----------- 清楚缓存信息 --------------
 ///删除登录用户信息
-- (void)SSclearNSuserDefault;
++ (void)SSclearNSuserDefault;
 
 ///清空documents路径下的文件及文件夹
-- (void)SSclearDocuments;
++ (void)SSclearDocuments;
 
 ///清空cache文件夹下的文件及文件夹
-- (void)SSclearCache;
++ (void)SSclearCache;
 
 #pragma mark --------- 计算文件大小 -------------
 ///documents路径下内容的大小
-- (unsigned long long)SSfileSizeForDocument;
++ (unsigned long long)SSfileSizeForDocument;
 ///cacahes路径下内容的大小
-- (unsigned long long)SSfileSizeForCaches;
++ (unsigned long long)SSfileSizeForCaches;
 
 ///将计算好的大小以 KB/MB/GB 形式展示
-- (NSString*)SSsizeStr:(unsigned long long)size;
++ (NSString*)SSsizeStr:(unsigned long long)size;
 
 @end
 
--- baseProject/common/manager/SSfileManager.m
+++ baseProject/common/manager/SSfileManager.m
@@ -21,54 +21,111 @@ static SSfileManager* fileManager = nil;
 
 #pragma mark ------------- 路径 ---------------
 ///沙盒中documents路径
-- (NSString*)SSdocumentsPath {
++ (NSString*)SSdocumentsPath {
     return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
 }
 
 ///沙盒中library路径
-- (NSString*)SSlibraryPath {
++ (NSString*)SSlibraryPath {
     return [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) lastObject];
 }
 
 ///沙盒中caches路径
-- (NSString*)SScachesPath {
++ (NSString*)SScachesPath {
     return [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject];
 }
 
 ///沙盒中temp路径
-- (NSString*)SStempPath {
++ (NSString*)SStempPath {
     return NSTemporaryDirectory();
 }
 
 #pragma mark ----------- 创建缓存文件夹/文件 -----------
 ///在caches路径下创建文件夹
-- (void)SScreateDirectoryWithCachesPath:(NSString*)directoryPath {
++ (void)SScreateDirectoryWithCachesPath:(NSString*)directoryPath {
     [[NSFileManager defaultManager] createDirectoryAtPath:[[self SScachesPath] stringByAppendingPathComponent:directoryPath] withIntermediateDirectories:YES attributes:nil error:nil];
 }
 
 ///在caches路径下创建文件
-- (void)SScreateFileWithCachesPath:(NSString*)filePath {
++ (void)SScreateFileWithCachesPath:(NSString*)filePath {
     [[NSFileManager defaultManager] createFileAtPath:[[self SScachesPath] stringByAppendingPathComponent:filePath] contents:nil attributes:nil];
 }
 
 ///在document路径下创建文件夹
-- (void)SScreateDirectoryWithDocumentPath:(NSString*)directoryPath {
++ (void)SScreateDirectoryWithDocumentPath:(NSString*)directoryPath {
     [[NSFileManager defaultManager] createDirectoryAtPath:[[self SSdocumentsPath] stringByAppendingPathComponent:directoryPath] withIntermediateDirectories:YES attributes:nil error:nil];
 }
 
 ///在document路径下创建文件
-- (void)SScreateFileWithDocumentPath:(NSString*)filePath {
++ (void)SScreateFileWithDocumentPath:(NSString*)filePath {
     [[NSFileManager defaultManager] createFileAtPath:[[self SScachesPath] stringByAppendingPathComponent:filePath] contents:nil attributes:nil];
+}
+
+#pragma mark ----------- 写入数据到文件中 -------------
+///将文本数据写入文件中
++ (void)SSwriteStrToFile:(NSString*)textStr filePath:(NSString*)filePath {
+    if (!textStr) {
+        return;
+    }
+    if (!filePath) {
+        return;
+    }
+    BOOL isSuccess = [textStr writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    //方式一：
+    if (isSuccess) {
+        SSLog(@"写入成功");
+    }
+    //方式二：
+    while (isSuccess == NO) {
+        isSuccess = [textStr writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    }
+}
+///将数组数据写入文件中
++ (void)SSwriteArrToFile:(NSArray*)arr filePath:(NSString*)filePath {
+    if (arr.count <= 0) {
+        return;
+    }
+    if (!filePath) {
+        return;
+    }
+    BOOL isSuccess = [arr writeToFile:filePath atomically:YES];
+    //方式一：
+    if (isSuccess) {
+        SSLog(@"写入成功");
+    }
+    //方式二：
+    while (isSuccess == NO) {
+        isSuccess = [arr writeToFile:filePath atomically:YES];
+    }
+    
+}
+///将字典数据写入文件中
++ (void)SSwirteDicToFile:(NSDictionary*)dic filePath:(NSString*)filePath {
+    if (dic.allKeys.count <= 0) {
+        return;
+    }
+    if (!filePath) {
+        return;
+    }
+    BOOL isSuccess = [dic writeToFile:filePath atomically:YES];
+    //方式一：
+    if (isSuccess) {
+        SSLog(@"写入成功");
+    }
+    //方式二：
+    while (isSuccess == NO) {
+        isSuccess = [dic writeToFile:filePath atomically:YES];
+    }
 }
 
 #pragma mark ----------- 清楚缓存信息 --------------
 ///删除登录用户信息
-- (void)SSclearNSuserDefault {
++ (void)SSclearNSuserDefault {
     [[NSUserDefaults standardUserDefaults] removeObjectForKey:token];
 }
 
 ///清空documents路径下的文件及文件夹
-- (void)SSclearDocuments {
++ (void)SSclearDocuments {
     //一
     NSArray* filePaths = [[NSFileManager defaultManager] subpathsAtPath:[self SSdocumentsPath]];
     for (NSString* fileP in filePaths) {
@@ -86,7 +143,7 @@ static SSfileManager* fileManager = nil;
 }
 
 ///清空cache文件夹下的文件及文件夹
-- (void)SSclearCache {
++ (void)SSclearCache {
     //一
     for (NSString* fileP in [[NSFileManager defaultManager] subpathsAtPath:[self SScachesPath]]) {
         NSError* error;
@@ -103,7 +160,7 @@ static SSfileManager* fileManager = nil;
 
 #pragma mark --------- 计算文件大小 -------------
 
-- (unsigned long long)SSfileSizeForDocument {
++ (unsigned long long)SSfileSizeForDocument {
     unsigned long long size = 0;
     if ([[[NSFileManager defaultManager] subpathsAtPath:[self SSdocumentsPath]] count] <= 0) {
         return size;
@@ -114,7 +171,7 @@ static SSfileManager* fileManager = nil;
     return size;
 }
 
-- (unsigned long long)SSfileSizeForCaches {
++ (unsigned long long)SSfileSizeForCaches {
     unsigned long long size = 0;
     if ([[[NSFileManager defaultManager] subpathsAtPath:[self SScachesPath]] count] <= 0) {
         return size;
@@ -126,7 +183,7 @@ static SSfileManager* fileManager = nil;
 }
 
 ///将计算好的大小以 KB/MB/GB 形式展示
-- (NSString*)SSsizeStr:(unsigned long long)size {
++ (NSString*)SSsizeStr:(unsigned long long)size {
     if (size < 1024) {
         return [NSString stringWithFormat:@"%lluB",size];
     }else if (size < 1024*1024) {
